ab16977deadc6a8a997d56dca48ce1c7d1534cf6
Eugene <frifster2014@gmail.com>
2023-04-30 19:20:57 +0800

    Declared Routes

---
+++ src/constants/routes.ts
@@ -0,0 +1,28 @@
+const ROUTES = {
+  MENU: {
+    NAME: "Our Menu",
+    PATH: "/menu",
+  },
+  ORDER_ONLINE: {
+    NAME: "Order Online",
+    PATH: "/order-online",
+  },
+  REWARDS: {
+    NAME: "Rewards",
+    PATH: "/rewards",
+  },
+  LOCATIONS: {
+    NAME: "Locations",
+    PATH: "/locations",
+  },
+  REACH_US: {
+    NAME: "Reach Us",
+    PATH: "/reach-us",
+  },
+  HOME: {
+    NAME: "Home",
+    PATH: "/",
+  },
+};
+
+export default ROUTES;
