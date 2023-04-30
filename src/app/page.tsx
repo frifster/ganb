a97ec6f4fad1fb7e698dc9ce96db9e709f89d2d8
Eugene <frifster2014@gmail.com>
2023-04-30 19:20:33 +0800

    clean layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import Button from '@components/Button'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
-      <Button>
-        Test Button
-      </Button>
-
-      <div> eyy</div>
+    <main>
     </main>
   )
 }
