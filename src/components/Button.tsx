41aa3ccaec1f3caafc6fe9ead6177ccf800134a3
Eugene <frifster2014@gmail.com>
2023-04-30 19:20:45 +0800

    Updated: Shared button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import styled from '@emotion/styled';
-import MQ from "@constants/media_queries";
 import { pulse } from '@constants/keyframes';
+// import MQ from "@constants/media_queries";
+
 
 const Button = styled.button`
-    background-color: #fff;
+    background-color: var(--accent-color-2);
     border: 1px solid #000;
     border-radius: 9999px;
     color: #000;
@@ -13,36 +14,17 @@ const Button = styled.button`
     font-weight: 600;
     padding: 0.3rem 0.5rem;
     box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
-    transition: all 0.2s ease-in-out;
+    width: 100%;
+    max-width: 333px;
+    text-transform: uppercase;
 
-    &:focus,
     &:hover {
-        color: rgba(255, 0, 0, 0.8);
-        
+        background-color: var(--accent-color);
+        color: var(--secondary-color);
     }
 
     &:active {
         animation: ${pulse} 500ms ease;
-    }
-
-    ${MQ[0]} {
-        color: blue;
-    }
-
-    ${MQ[1]} {
-        color: green;
-    }
-
-    ${MQ[2]} {
-        color: orange;
-    }
-
-    ${MQ[3]} {
-        color: pink;
-    }
-
-    ${MQ[4]} {
-        color: black;
     }
 `;
 
