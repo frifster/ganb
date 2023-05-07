f3c8ac25b28103d41ea3d444b04e618c40075bf5
Eugene <frifster2014@gmail.com>
2023-05-07 15:22:44 +0800

    - changed page.tsx to sass dependent component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-"use client";
-import styled from '@emotion/styled';
 import FrappeSection from './FrappeSection';
 import EggdropSection from './EggdropSection';
 
 export default function Home() {
 
   return (
-    <StyledMain>
+    <>
       <FrappeSection />
       <EggdropSection />
-    </StyledMain>
+    </>
   )
 }
-
-const StyledMain = styled.main({
-  padding: "120px 0 0 0"
-})
