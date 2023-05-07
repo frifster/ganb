dcaa3f21ab80d4c3e29d4f18ac2d5b7649194996
Eugene <frifster2014@gmail.com>
2023-05-07 15:31:18 +0800

    changed not-found page styles to tailwind css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,30 +1,14 @@
-"use client";
-import styled from "@emotion/styled";
 import Image from "next/image";
 import ConstructionImg from "@assets/images/underconstruction.png"
 
 export default function NotFound() {
     return (
-        <Container>
+        <div className="grid place-items-center text-center text-gray-500 px-24">
             <div>
                 <Image src={ConstructionImg} alt="404" />
-                <h2>This page is currently under construction.</h2>
+                <h2 className="font-bold">This page is currently under construction.</h2>
                 <p>Sorry for the inconvenience</p>
             </div>
-        </Container>
+        </div>
     );
 }
-
-
-const Container = styled.div({
-    padding: '0 6dvw',
-    display: 'grid',
-    placeItems: 'center',
-    minHeight: "94vh",
-    textAlign: 'center',
-    color: "gray",
-
-    h2: {
-        fontWeight: 700,
-    }
-});
