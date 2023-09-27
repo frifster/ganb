8fc8ff380e5aac5b5f1b5d823028d2f9daff0368
Eugene Avila <frifster2014@gmail.com>
2023-09-27 18:23:02 +0800

    Added menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,22 @@
-import styles from './menu.module.scss';
+"use client";
+import HotMenuImg from "@assets/images/hotmenu.png";
+import MatchaImg from "@assets/images/matcha.png";
+import MenuImg from "@assets/images/menu.png";
+import PerfectComboImg from "@assets/images/perfectcombo.png";
+import SuperComboImg from "@assets/images/supercombo.jpg";
+import Image from "next/image";
+import styles from "./menu.module.scss";
 
 const Menu = () => {
-    return (
-        <div className={styles.menu} >
-            <h1>Menu</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.menu}>
+      <Image src={MenuImg} alt="Grind and Blend Menu" />
+      <Image src={HotMenuImg} alt="Grind and Blend Hot Menu" />
+      <Image src={MatchaImg} alt="Grind and Blend Matcha" />
+      <Image src={PerfectComboImg} alt="Grind and Blend Perfect Combo" />
+      <Image src={SuperComboImg} alt="Grind and Blend Super Combo" />
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
