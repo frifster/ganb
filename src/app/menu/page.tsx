0f585a956f97d9bd0ebbfaad68bd34367b1039a6
Eugene Avila <frifster2014@gmail.com>
2023-11-13 23:35:19 +0800

    Removed other images

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import HotMenuImg from "@assets/images/hotmenu.png";
-import MatchaImg from "@assets/images/matcha.png";
 import MenuImg from "@assets/images/newmenu.png";
-import PerfectComboImg from "@assets/images/perfectcombo.png";
-import SuperComboImg from "@assets/images/supercombo.jpg";
 import Image from "next/image";
 import styles from "./menu.module.scss";
 
@@ -11,10 +7,6 @@ const Menu = () => {
   return (
     <div className={styles.menu}>
       <Image src={MenuImg} alt="Grind and Blend Menu" />
-      <Image src={HotMenuImg} alt="Grind and Blend Hot Menu" />
-      <Image src={MatchaImg} alt="Grind and Blend Matcha" />
-      <Image src={PerfectComboImg} alt="Grind and Blend Perfect Combo" />
-      <Image src={SuperComboImg} alt="Grind and Blend Super Combo" />
     </div>
   );
 };
