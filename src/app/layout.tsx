747fe10179309f31285a0d15ea2d588ea7c7dd2f
Eugene Avila <frifster2014@gmail.com>
2024-05-14 07:52:55 +0800

    Integrate ClerkProvider into RootLayout
    The main change involves the inclusion of the ClerkProvider component into the RootLayout component. This update also organizes the import statements, replacing React.ReactNode with ReactNode for cleaner syntax, and adjusts formatting for readability and consistency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,43 @@
-import Head from "next/head"
-import { Analytics } from '@vercel/analytics/react';
-import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import Footer from "@components/Layout/Footer";
 
-import NavBar from '@components/Layout/NavBar'
-import Footer from '@components/Layout/Footer';
-
-import './globals.css'
+import "./globals.css";
 import MainContainer from "@components/Layout/MainContainer";
 
-const inter = Inter({ subsets: ['latin'] })
+import NavBar from "@components/Layout/NavBar";
+import { Analytics } from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { ReactNode } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Grind & Blend',
-  description: 'Grind and Blend website',
-}
+  title: "Grind & Blend",
+  description: "Grind and Blend website"
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: ReactNode;
 }
 
 export default function RootLayout(props: RootLayoutProps) {
 
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body className={inter.className}>
-        <Analytics />
-        <NavBar />
-        <MainContainer>
-          {props.children}
-        </MainContainer>
-        <Footer />
+      <Analytics />
+      <NavBar />
+      <MainContainer>
+        {props.children}
+      </MainContainer>
+      <Footer />
       </body>
-    </html>
-  )
+      </html>
+    </ClerkProvider>
+  );
 }
