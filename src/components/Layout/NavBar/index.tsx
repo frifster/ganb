0083dd2587a96add42a836168bc61979c2983df8
Eugene Avila <frifster2014@gmail.com>
2024-05-14 08:41:03 +0800

    Add authentication features to NavBar
    Implemented SignIn and UserButton components in NavBar for both mobile and desktop versions. These features will allow users to log in from the Navbar directly, improving accessibility and user experience.

--- src/components/Layout/NavBar/index.tsx
+++ src/components/Layout/NavBar/index.tsx
@@ -1,77 +1,94 @@
 "use client";
-import { useState } from 'react';
-import { useScroll, motion, useTransform } from "framer-motion";
-import Image from 'next/image';
-import Link from 'next/link';
-import classnames from 'classnames/bind';
+import Burger from "@assets/svgs/burger.svg";
 
-import Logo from '@assets/svgs/logo.svg';
-import Burger from '@assets/svgs/burger.svg';
-import ROUTES from '@constants/routes';
+import Logo from "@assets/svgs/logo.svg";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ROUTES from "@constants/routes";
+import classnames from "classnames/bind";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 import styles from "./navbar.module.scss";
 
 const NAV_ROUTES = [
-    { ...ROUTES.MENU },
-    { ...ROUTES.ORDER_ONLINE },
-    { ...ROUTES.REWARDS },
-    { ...ROUTES.LOCATIONS },
-    { ...ROUTES.REACH_US }
-]
+  { ...ROUTES.MENU },
+  { ...ROUTES.ORDER_ONLINE },
+  { ...ROUTES.REWARDS },
+  { ...ROUTES.LOCATIONS },
+  { ...ROUTES.REACH_US }
+];
 
 
 const cx = classnames.bind(styles);
 
 const NavBar = () => {
-    const { scrollYProgress } = useScroll()
-    const backgroundColor = useTransform(scrollYProgress, [0, 0.1, 1], ['rgba(245, 222, 179,0)', 'rgba(245, 222, 179,0.8)', 'rgba(245, 222, 179,1)'])
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { scrollYProgress } = useScroll();
+  const backgroundColor = useTransform(scrollYProgress, [0, 0.1, 1], ["rgba(245, 222, 179,0)", "rgba(245, 222, 179,0.8)", "rgba(245, 222, 179,1)"]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const handleOverlayClick = () => {
-        setIsMenuOpen(false);
-    }
+  const handleOverlayClick = () => {
+    setIsMenuOpen(false);
+  };
 
-    const handleBurgerClick = () => {
-        setIsMenuOpen(true);
-    }
+  const handleBurgerClick = () => {
+    setIsMenuOpen(true);
+  };
 
-    const overlayClass = cx(styles.overlay, { isMenuOpen })
-    const mobileNavMenuClass = cx(styles.mobile_nav_menu, { isMenuOpen })
+  const overlayClass = cx(styles.overlay, { isMenuOpen });
+  const mobileNavMenuClass = cx(styles.mobile_nav_menu, { isMenuOpen });
 
-    return (
-        <motion.nav style={{ backgroundColor }} className={styles.navbar}>
-            <div className={styles.desktop_nav_menu}>
-                {
-                    NAV_ROUTES.map((route, index) => (
-                        <div className={styles.desktop_nav_menu_item} key={route.NAME + index} >
-                            <Link href={route.PATH}>
-                                {route.NAME}
-                            </Link>
-                        </div>
-                    ))
-                }
+  return (
+    <motion.nav style={{ backgroundColor }} className={styles.navbar}>
+      <div className={styles.desktop_nav_menu}>
+        <div>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
+        {
+          NAV_ROUTES.map((route, index) => (
+            <div className={styles.desktop_nav_menu_item} key={route.NAME + index}>
+              <Link href={route.PATH}>
+                {route.NAME}
+              </Link>
             </div>
-            <div className={styles.burger_menu}>
-                <Image src={Burger} alt="Burger Menu" onClick={handleBurgerClick} />
-                <div className={overlayClass} onClick={handleOverlayClick} />
-                <div className={mobileNavMenuClass}>
-                    {
-                        NAV_ROUTES.map((route, index) => (
-                            <div className={styles.mobile_nav_menu_item} key={route.NAME + index}>
-                                <Link href={route.PATH}>
-                                    {route.NAME}
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+          ))
+        }
+      </div>
+      <div className={styles.burger_menu}>
+        <Image src={Burger} alt="Burger Menu" onClick={handleBurgerClick} />
+        <div className={overlayClass} onClick={handleOverlayClick} />
+        <div className={mobileNavMenuClass}>
+          {
+            NAV_ROUTES.map((route, index) => (
+              <div className={styles.mobile_nav_menu_item} key={route.NAME + index}>
+                <Link href={route.PATH}>
+                  {route.NAME}
+                </Link>
+              </div>
+            ))
+          }
+          <div className={styles.mobile_nav_menu_item}>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </div>
+        </div>
+      </div>
 
-            <Link href={ROUTES.HOME.PATH} className={styles.home_logo}>
-                <Image priority src={Logo} alt="Grind and Blend Logo" />
-                <h1>Grind & Blend</h1>
-            </Link>
-        </motion.nav>
-    )
-}
+      <Link href={ROUTES.HOME.PATH} className={styles.home_logo}>
+        <Image priority src={Logo} alt="Grind and Blend Logo" />
+        <h1>Grind & Blend</h1>
+      </Link>
+    </motion.nav>
+  );
+};
 
 export default NavBar;
